e0294026e28812612337c9b42c5ab986bbf9df10
andrewjuarez <a777juarez@gmail.com>
2019-03-18 18:12:36 -0700

    Updates

---
+++ precache-manifest.e5d028d04f0229a3532ae5352c15e1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d0711f1f86d447db4c1e07a5914ce77",
+    "url": "/static/media/headshot-cropped.4d0711f1.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d28e70b5fdc36e5ce770",
+    "url": "/static/js/main.d28e70b5.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "d28e70b5fdc36e5ce770",
+    "url": "/static/css/main.b5651993.chunk.css"
+  },
+  {
+    "revision": "31f2b9eb8e131af0c89aaf772198bb81",
+    "url": "/index.html"
+  }
+];
